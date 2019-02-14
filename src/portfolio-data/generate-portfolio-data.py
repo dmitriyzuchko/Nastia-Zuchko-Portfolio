096ac1c41e8a76f3ba5339e9da08845410c045c0
Dmitriy Zuchko <dmitriyzuchko@gmail.com>
2019-02-13 20:21:51 -0500

    Modefied the portfolio_data generator script to match the new file scheme

--- src/portfolio-data/generate-portfolio-data.py
+++ src/portfolio-data/generate-portfolio-data.py
@@ -3,36 +3,44 @@ import json
 import pprint
 
 sub_path = '../../public/'
-images = glob.glob(sub_path + 'portfolio-pieces/**/*.jpg', recursive=True)
-text_files = glob.glob(sub_path + 'portfolio-pieces/**/*.txt')
+path = sub_path + 'portfolio-pieces/**/'
+image_path_patterns = (path + '*.png', path + '*.jpg')
+
+image_paths = []
+for path in image_path_patterns:
+    image_paths.extend(glob.glob(path, recursive=True))
+
+text_paths = glob.glob(sub_path + 'portfolio-pieces/**/*.txt', recursive=True)
 
 portfolio_data = {}
 
-for image in images:
-    seperated_path = image.split('/')
+for image_path in image_paths:
+    seperated_path = image_path.split('/')
     row_id = seperated_path[4]
 
-    piece_id = seperated_path[5].split('.')[0]
-    
+    piece_id = seperated_path[5]
+
     if row_id not in portfolio_data:
         portfolio_data[row_id] = {}
-    
+
     if piece_id not in portfolio_data[row_id]:
         portfolio_data[row_id][piece_id] = {'urls': []}
-    
-    portfolio_data[row_id][piece_id]['urls'].append(image[len(sub_path):])
 
-for text_file in text_files: 
-    seperated_path = text_file.split('/')
+    portfolio_data[row_id][piece_id]['urls'].append(image_path[len(sub_path):])
+
+for text_path in text_paths:
+    seperated_path = text_path.split('/')
     row_id = seperated_path[4]
-    id_and_description = seperated_path[5].split('.')[0]
-    piece_id = id_and_description.split('-')[0]
-    description = id_and_description.split('-')[1]
+    piece_id = seperated_path[5]
+    file_name = seperated_path[6].split('.')[0]
 
-    with open(text_file, 'r') as text_data:
+    with open(text_path, 'r') as text_data:
         text = text_data.read()
-    
-    portfolio_data[row_id][piece_id][description] = text
+
+    if file_name == 'description':
+        portfolio_data[row_id][piece_id]['description'] = text
+    else:
+        portfolio_data[row_id][piece_id]['name'] = text
 
 sorted_portfolio = []
 
@@ -49,15 +57,18 @@ for row_key in portfolio_data:
             'description': old_piece['description'],
             'name': url_friendly_name,
             'urls': old_piece['urls']
-            }
+        }
 
         piece['urls'] = sorted(piece['urls'])
         new_format['items'].append(piece)
 
-    new_format['items'] = sorted(new_format['items'], key=lambda item: item['piece_id'])
-    sorted_portfolio.append(new_format)    
+    new_format['items'] = sorted(
+        new_format['items'], key=lambda item: item['piece_id'])
+    sorted_portfolio.append(new_format)
 
 sorted_portfolio = sorted(sorted_portfolio, key=lambda row: row['row_id'])
 
 with open('portfolio_data.json', 'w') as portfolio_file:
     json.dump(sorted_portfolio, portfolio_file)
+
+pprint.pprint(sorted_portfolio)
