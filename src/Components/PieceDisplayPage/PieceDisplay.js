c1b15fc75de4b58b4705ee9dbf105c73819bd98f
Dmitriy Zuchko <dmitriyzuchko@gmail.com>
2019-03-27 00:35:44 -0400

    Extracted PieceDisplay from PieceDisplayPage

---
+++ src/Components/PieceDisplayPage/PieceDisplay.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import SupportingImageButton from './SupportingImageButton';
+
+export class PieceDisplay extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { selectedThumbnail: [], orientation: 'landscape' };
+
+        this.setDisplayOrientationClass = this.setDisplayOrientationClass.bind(
+            this
+        );
+        this.changeImageDisplay = this.changeImageDisplay.bind(this);
+    }
+
+    componentWillMount() {
+        this.setDisplayOrientationClass();
+        this.setThumbnailState();
+    }
+
+    setDisplayOrientationClass() {
+        if (this.props.pieceData.orientation === 'portrait') {
+            this.state.orientation = 'portrait';
+        }
+    }
+
+    setThumbnailState() {
+        const supportingImages = this.props.pieceData.urls;
+
+        supportingImages.map((_, index) => {
+            if (index === 0) {
+                this.state.selectedThumbnail.push(true);
+            } else {
+                this.state.selectedThumbnail.push(false);
+            }
+        });
+    }
+
+    changeImageDisplay(index, imageSrc) {
+        let imageDisplay = document.querySelector('#piece-image img');
+
+        imageDisplay.src = imageSrc;
+
+        // Only one thumbnail can be highlighted or "on" at any time.
+        this.setState({
+            selectedThumbnail: this.state.selectedThumbnail.map((_, i) =>
+                i === index ? true : false
+            )
+        });
+
+        console.log(this.state.selectedThumbnail);
+    }
+
+    render() {
+        let mainImage = `/${this.props.pieceData['urls'][0]}`;
+        const description = this.props.pieceData.description;
+        const name = this.props.pieceData.name;
+        const supportingImages = this.props.pieceData['urls'];
+
+        let supportingImageHTML = [];
+
+        if (supportingImages.length > 1) {
+            supportingImageHTML = supportingImages.map((image, index) => {
+                const imageSrc = `/${image}`;
+
+                return (
+                    <SupportingImageButton
+                        imageSrc={imageSrc}
+                        key={index}
+                        isActive={this.state.selectedThumbnail[index]}
+                        onClick={imageSrc =>
+                            this.changeImageDisplay(index, imageSrc)
+                        }
+                    />
+                );
+            });
+        }
+
+        const displayClass =
+            this.state.orientation === 'landscape'
+                ? 'landscape-display'
+                : 'portrait-display';
+        const imageClass =
+            this.state.orientation === 'landscape'
+                ? 'landscape-image'
+                : 'portrait-image';
+
+        return (
+            <div id='piece-display' className={displayClass}>
+                <div id='piece-image' className={imageClass}>
+                    <img src={mainImage} alt={name} />
+                </div>
+                <div id='details-display'>
+                    <div id='supporting-image-collection'>
+                        {supportingImageHTML}
+                    </div>
+                    <div id='description'>&emsp;{description}</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default PieceDisplay;
--- src/Components/PieceDisplayPage/PieceDisplayPage.js
+++ src/Components/PieceDisplayPage/PieceDisplayPage.js
@@ -1,43 +1,18 @@
 import React, { Component } from 'react';
 import PortfolioData from '../../portfolio-data/portfolio_data.json';
 import PieceNavBar from './PieceNavBar/PieceNavBar.js';
+import PieceDisplay from './PieceDisplay.js';
 import Footer from '../Footer';
 import './PieceDisplay.scss';
-import SupportingImageButton from './SupportingImageButton.js';
 
 class PieceDisplayPage extends Component {
     pieceData;
     constructor(props) {
         super(props);
-
-        this.state = { selectedThumbnail: [], orientation: 'landscape' };
-        this.changeImageDisplay = this.changeImageDisplay.bind(this);
     }
 
     componentWillMount() {
         this.obtainPieceData();
-        this.setDisplayOrientationClass();
-    }
-
-    setDisplayOrientationClass() {
-        if (this.pieceData.orientation === 'portrait') {
-            this.state.orientation = 'portrait';
-        }
-    }
-
-    changeImageDisplay(index, imageSrc) {
-        let imageDisplay = document.querySelector('#piece-image img');
-
-        imageDisplay.src = imageSrc;
-
-        // Only one thumbnail can be highlighted or "on" at any time.
-        this.setState({
-            selectedThumbnail: this.state.selectedThumbnail.map((_, i) =>
-                i === index ? true : false
-            )
-        });
-
-        console.log(this.state.selectedThumbnail);
     }
 
     changeDisplayedPiece(direction) {
@@ -67,67 +42,16 @@ class PieceDisplayPage extends Component {
 
         if (resultFound) {
             this.pieceData = pieceData;
-
-            pieceData.urls.map((_, index) => {
-                if (index !== 0) {
-                    this.state.selectedThumbnail.push(false);
-                } else {
-                    this.state.selectedThumbnail.push(true);
-                }
-            });
         } else {
             this.props.history.push('/no-match');
         }
     }
 
     render() {
-        let mainImage = `/${this.pieceData['urls'][0]}`;
-        const description = this.pieceData.description;
-        const name = this.pieceData.name;
-        const supportingImages = this.pieceData['urls'];
-
-        let supportingImageHTML = [];
-
-        if (supportingImages.length > 1) {
-            supportingImageHTML = supportingImages.map((image, index) => {
-                const imageSrc = `/${image}`;
-
-                return (
-                    <SupportingImageButton
-                        imageSrc={imageSrc}
-                        key={index}
-                        isActive={this.state.selectedThumbnail[index]}
-                        onClick={imageSrc =>
-                            this.changeImageDisplay(index, imageSrc)
-                        }
-                    />
-                );
-            });
-        }
-
-        const displayClass =
-            this.state.orientation === 'landscape'
-                ? 'landscape-display'
-                : 'portrait-display';
-        const imageClass =
-            this.state.orientation === 'landscape'
-                ? 'landscape-image'
-                : 'portrait-image';
-
         return (
             <>
                 <PieceNavBar navigate={this.changeDisplayedPiece} />
-                <div id='piece-display' className={displayClass}>
-                    <div id='piece-image' className={imageClass}>
-                        <img src={mainImage} alt={name} />
-                    </div>
-                    <div id='details-display'>
-                        <div id='supporting-image-collection'>
-                            {supportingImageHTML}
-                        </div>
-                        <div id='description'>&emsp;{description}</div>
-                    </div>
-                </div>
+                <PieceDisplay pieceData={this.pieceData} />
                 <Footer />
             </>
         );
