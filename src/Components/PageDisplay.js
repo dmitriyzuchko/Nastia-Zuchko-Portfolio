12e2576745a7adcf54027e11fcc6b7f293b7c989
Dmitriy Zuchko <dmitriyzuchko@gmail.com>
2019-04-20 18:44:29 -0400

    Refactored to functional component

--- src/Components/PageDisplay.js
+++ src/Components/PageDisplay.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import SiteHead from './SiteHead/SiteHead';
 import Footer from './Footer';
@@ -7,74 +7,74 @@ import Contact from './Contact/Contact';
 import NoMatch from './NoMatch';
 import PieceDisplayPage from './PieceDisplayPage/PieceDisplayPage';
 
-class PageDisplay extends Component {
-    previousLocation = this.props.location;
+const lockBodyScroll = shouldLock => {
+    if (shouldLock) {
+        const htmlTag = document.querySelector('html');
 
-    componentWillUpdate(nextProps) {
-        let { location } = this.props;
+        document.body.style.overflowY = 'hidden';
+        htmlTag.style.overflowY = 'hidden';
+        // Necessary for Safari to treat the background as unscrollable
+        htmlTag.style.height = '100%';
+    } else {
+        const htmlTag = document.querySelector('html');
 
-        if (
-            nextProps.history.action !== 'POP' &&
-            (!location.state || !location.state.modal)
-        ) {
-            this.previousLocation = this.props.location;
-        }
+        document.body.style.overflowY = 'auto';
+        htmlTag.style.overflowY = 'auto';
+        htmlTag.style.height = 'auto';
     }
+};
 
-    render() {
-        let { location } = this.props;
+const PageDisplay = props => {
+    const [previousLocation, setPreviousLocation] = useState(props.location);
+    const { location } = props;
 
-        const isModal = !!(
-            location.state &&
-            location.state.modal &&
-            this.previousLocation !== location &&
-            !(/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream)
-        );
+    useEffect(() => {
+        const originatesFromPortfolio =
+            props.history.action !== 'POP' &&
+            (!location.state || !location.state.modal);
 
-        const isNotPortfolioPiece = !location.pathname.startsWith(
-            '/portfolio-piece'
-        );
-
-        if (isModal) {
-            const htmlTag = document.querySelector('html');
-
-            document.body.style.overflowY = 'hidden';
-            htmlTag.style.overflowY = 'hidden';
-            // Necessary for Safari to treat the background as unscrollable
-            htmlTag.style.height = '100%';
-        } else {
-            const htmlTag = document.querySelector('html');
-
-            document.body.style.overflowY = 'auto';
-            htmlTag.style.overflowY = 'auto';
-            htmlTag.style.height = 'auto';
+        if (originatesFromPortfolio) {
+            setPreviousLocation(location);
         }
+    }, [location]);
 
-        return (
-            <>
-                {isNotPortfolioPiece || isModal ? <SiteHead /> : null}
-                <Switch location={isModal ? this.previousLocation : location}>
-                    <Route exact path='/' component={PortfolioDisplay} />
-                    <Route path='/contact' component={Contact} />
-                    <Route
-                        path='/portfolio-piece/:name'
-                        component={PieceDisplayPage}
-                    />
-                    <Route path='/404' component={NoMatch} />
-                    <Route component={NoMatch} />
-                </Switch>
-                <Footer />
-                {isModal ? (
-                    <Route
-                        path='/portfolio-piece/:name'
-                        render={routerProps => (
-                            <PieceDisplayPage isModal {...routerProps} />
-                        )}
-                    />
-                ) : null}
-            </>
-        );
-    }
-}
+    const isModal = !!(
+        location.state &&
+        location.state.modal &&
+        previousLocation !== location &&
+        !(/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream)
+    );
+
+    const isNotPortfolioPiece = !location.pathname.startsWith(
+        '/portfolio-piece'
+    );
+
+    lockBodyScroll(isModal);
+
+    return (
+        <>
+            {(isNotPortfolioPiece || isModal) && <SiteHead />}
+            <Switch location={isModal ? previousLocation : location}>
+                <Route exact path='/' component={PortfolioDisplay} />
+                <Route path='/contact' component={Contact} />
+                <Route
+                    path='/portfolio-piece/:name'
+                    component={PieceDisplayPage}
+                />
+                <Route path='/404' component={NoMatch} />
+                <Route component={NoMatch} />
+            </Switch>
+            <Footer />
+            {isModal && (
+                <Route
+                    path='/portfolio-piece/:name'
+                    render={routerProps => (
+                        <PieceDisplayPage isModal {...routerProps} />
+                    )}
+                />
+            )}
+        </>
+    );
+};
 
 export default PageDisplay;
