67a264e8e78ae1163e4fb05fee9a50ae0c6646bc
Dmitriy Zuchko <dmitriyzuchko@gmail.com>
2019-04-20 19:16:50 -0400

    Refactored to functional component

--- src/Components/PieceDisplayPage/SupportingImageButton.js
+++ src/Components/PieceDisplayPage/SupportingImageButton.js
@@ -1,28 +1,13 @@
 import React, { Component } from 'react';
 
-export class SupportingImageButton extends Component {
-    constructor(props) {
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick() {
-        this.props.onClick(`${this.props.imageSrc}`);
-    }
-
-    render() {
-        let activityClass = this.props.isActive
-            ? 'active-thumb'
-            : 'inactive-thumb';
-
-        return (
-            <div
-                className={activityClass}
-                style={{ backgroundImage: `url('${this.props.imageSrc}')` }}
-                onClick={this.handleClick}
-            />
-        );
-    }
-}
+const SupportingImageButton = props => {
+    return (
+        <div
+            className={props.isActive ? 'active-thumb' : 'inactive-thumb'}
+            style={{ backgroundImage: `url('${props.imageSrc}')` }}
+            onClick={() => props.onClick(`${props.imageSrc}`)}
+        />
+    );
+};
 
 export default SupportingImageButton;
