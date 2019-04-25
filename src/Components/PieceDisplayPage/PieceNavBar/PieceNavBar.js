210850c92c7e68998b7efea6d0ee241cc2a70dc5
Dmitriy Zuchko <dmitriyzuchko@gmail.com>
2019-04-24 23:24:50 -0400

    Refactored to functional component

--- src/Components/PieceDisplayPage/PieceNavBar/PieceNavBar.js
+++ src/Components/PieceDisplayPage/PieceNavBar/PieceNavBar.js
@@ -1,41 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 
-class PieceNavBar extends Component {
-    constructor(props) {
-        super(props);
+const PieceNavBar = props => {
+    const isModal = props.isModal;
+    const buttonIcon = isModal ? 'fas fa-times' : 'fas fa-home';
 
-        this.handleNavigation = this.handleNavigation.bind(this);
-    }
-
-    handleNavigation(e) {
-        if (this.props.isModal) {
-            this.props.history.goBack();
+    const handleNavigation = e => {
+        if (isModal) {
+            props.history.goBack();
             // Enables goBack() to work in Safari.
             e.preventDefault();
         } else {
-            this.props.history.push('/');
+            props.history.push('/');
         }
-    }
+    };
 
-    render() {
-        const buttonIcon = this.props.isModal ? 'fas fa-times' : 'fas fa-home';
-
-        return (
-            <div id='piece-nav-bar'>
-                <button onClick={this.handleNavigation}>
-                    <div className='home-button'>
-                        <i className={buttonIcon} />
-                    </div>
-                </button>
-                <div id='nav-title'>
-                    <button onClick={this.handleNavigation}>
-                        nastia zuchko
-                    </button>
+    return (
+        <div id='piece-nav-bar'>
+            <button onClick={handleNavigation}>
+                <div className='home-button'>
+                    <i className={buttonIcon} />
                 </div>
+            </button>
+            <div id='nav-title'>
+                <button onClick={handleNavigation}>nastia zuchko</button>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default withRouter(PieceNavBar);
