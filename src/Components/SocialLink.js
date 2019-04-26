259ae96ca841ee13aa61512f1d0a5dc4d60ed6d6
DmitriyZ <dmitriyzuchko@gmail.com>
2019-04-25 21:25:52 -0400

    Converted to functional component

--- src/Components/SocialLink.js
+++ src/Components/SocialLink.js
@@ -1,44 +1,42 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export class SocialLink extends Component {
-  render() {
-    let social_network_url = "";
-    let class_name = "fab ";
+const SocialLink = props => {
+    let socialNetworkUrl = '';
+    let className = 'fab ';
 
-    switch (this.props.socialNetwork) {
-      case "twitter":
-        social_network_url += "https://twitter.com/{x}";
-        class_name += "fa-twitter";
-        break;
-      case "tumblr":
-        social_network_url += "https://tumbrl.com/{x}";
-        class_name += "fa-tumblr";
-        break;
-      case "instagram":
-        social_network_url += "https://instagram.com/{x}";
-        class_name += "fa-instagram";
-        break;
-      case "wordpress":
-        social_network_url += "https://{x}.wordpress.com";
-        class_name += "fa-wordpress";
-        break;
-      default:
+    switch (props.socialNetwork) {
+        case 'twitter':
+            socialNetworkUrl += 'https://twitter.com/{x}';
+            className += 'fa-twitter';
+            break;
+        case 'tumblr':
+            socialNetworkUrl += 'https://tumbrl.com/{x}';
+            className += 'fa-tumblr';
+            break;
+        case 'instagram':
+            socialNetworkUrl += 'https://instagram.com/{x}';
+            className += 'fa-instagram';
+            break;
+        case 'wordpress':
+            socialNetworkUrl += 'https://{x}.wordpress.com';
+            className += 'fa-wordpress';
+            break;
+        default:
     }
 
-    social_network_url = social_network_url.replace("{x}", this.props.username);
+    socialNetworkUrl = socialNetworkUrl.replace('{x}', props.username);
 
     return (
-      <a href={social_network_url} className="social-network-link">
-        <i className={class_name} />
-      </a>
+        <a href={socialNetworkUrl} className='social-network-link'>
+            <i className={className} />
+        </a>
     );
-  }
-}
+};
 
 SocialLink.propTypes = {
-  socialNetwork: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired
+    socialNetwork: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired
 };
 
 export default SocialLink;
