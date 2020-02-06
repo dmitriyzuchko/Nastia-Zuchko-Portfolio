315a1475fc932fa5426b79d64b1b1d0c4d53c56f
Dmitriy Zuchko <dmitriyzuchko@gmail.com>
2020-02-05 19:58:25 -0500

    Updated usernames for social network links

--- src/Components/SiteHead/SiteHead.js
+++ src/Components/SiteHead/SiteHead.js
@@ -5,52 +5,43 @@ import SocialLink from '../SocialLink';
 import './SiteHead.scss';
 
 const SiteHead = () => {
-    return (
-        <header>
-            <div className='container'>
-                <Link id='site-logo-wrapper' to='/'>
-                    <img id='site-logo' src={HeaderLogo} alt='Website logo' />
-                </Link>
-                <nav>
-                    <div id='internal-links'>
-                        <NavLink className='internal-link' exact to='/'>
-                            Portfolio
-                        </NavLink>
-                        <NavLink className='internal-link' to='/contact'>
-                            Contact
-                        </NavLink>
-                    </div>
-                    <div id='shop-links-wrapper'>
-                        <div id='shop-links'>
-                            <a
-                                className='shop-link'
-                                href='https://www.inprnt.com/gallery/nastiazuchko/'
-                            >
-                                Prints
-                            </a>
-                            <i className='fas fa-shopping-bag' />
-                            <a
-                                className='shop-link'
-                                href='https://society6.com/nastiazuchko'
-                            >
-                                Merch
-                            </a>
-                        </div>
-                    </div>
-                    <div id='external-links'>
-                        <SocialLink
-                            socialNetwork='twitter'
-                            username='nastiazuchko'
-                        />
-                        <SocialLink
-                            socialNetwork='instagram'
-                            username='nastiazuchko'
-                        />
-                    </div>
-                </nav>
+  return (
+    <header>
+      <div className='container'>
+        <Link id='site-logo-wrapper' to='/'>
+          <img id='site-logo' src={HeaderLogo} alt='Website logo' />
+        </Link>
+        <nav>
+          <div id='internal-links'>
+            <NavLink className='internal-link' exact to='/'>
+              Portfolio
+            </NavLink>
+            <NavLink className='internal-link' to='/contact'>
+              Contact
+            </NavLink>
+          </div>
+          <div id='shop-links-wrapper'>
+            <div id='shop-links'>
+              <a
+                className='shop-link'
+                href='https://www.inprnt.com/gallery/nastiazuchko/'
+              >
+                Prints
+              </a>
+              <i className='fas fa-shopping-bag' />
+              <a className='shop-link' href='https://society6.com/nastiazuchko'>
+                Merch
+              </a>
             </div>
-        </header>
-    );
+          </div>
+          <div id='external-links'>
+            <SocialLink socialNetwork='twitter' username='bardicious' />
+            <SocialLink socialNetwork='instagram' username='bardicious' />
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default SiteHead;
