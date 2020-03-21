96b581922197f41e939483361ff4f66206126eeb
Dmitriy Zuchko <dmitriyzuchko@gmail.com>
2020-03-21 14:32:10 -0400

    Updated PieceDisplay title

--- src/Components/PieceDisplayPage/PieceNavBar/PieceNavBar.js
+++ src/Components/PieceDisplayPage/PieceNavBar/PieceNavBar.js
@@ -2,31 +2,31 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 const PieceNavBar = props => {
-    const isModal = props.isModal;
-    const buttonIcon = isModal ? 'fas fa-times' : 'fas fa-home';
+  const isModal = props.isModal;
+  const buttonIcon = isModal ? 'fas fa-times' : 'fas fa-home';
 
-    const handleNavigation = e => {
-        if (isModal) {
-            props.history.goBack();
-            // Enables goBack() to work in Safari.
-            e.preventDefault();
-        } else {
-            props.history.push('/');
-        }
-    };
+  const handleNavigation = e => {
+    if (isModal) {
+      props.history.goBack();
+      // Enables goBack() to work in Safari.
+      e.preventDefault();
+    } else {
+      props.history.push('/');
+    }
+  };
 
-    return (
-        <div id='piece-nav-bar'>
-            <button onClick={handleNavigation}>
-                <div className='home-button'>
-                    <i className={buttonIcon} />
-                </div>
-            </button>
-            <div id='nav-title'>
-                <button onClick={handleNavigation}>nastia zuchko</button>
-            </div>
+  return (
+    <div id='piece-nav-bar'>
+      <button onClick={handleNavigation}>
+        <div className='home-button'>
+          <i className={buttonIcon} />
         </div>
-    );
+      </button>
+      <div id='nav-title'>
+        <button onClick={handleNavigation}>BARDICIOUS</button>
+      </div>
+    </div>
+  );
 };
 
 export default withRouter(PieceNavBar);
